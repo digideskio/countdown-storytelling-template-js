e2a25212a3df651cf07b8f41415cde13604192be
Lee Bock <lbock@esri.com>
2013-04-16 12:04:51 -0400

    populate the scroll list with actual items.

--- app/main.js
+++ app/main.js
@@ -10,6 +10,7 @@ dojo.require("esri.map");
 var TITLE = "World's Largest Container Ports"
 var BYLINE = "This is the byline"
 var WEBMAP_ID = "3732b8a6d0bc4a09b00247e8daf69af8";
+var LOCATIONS_LAYER_TITLE = "Largest Container Ports";
 var GEOMETRY_SERVICE_URL = "http://tasks.arcgisonline.com/ArcGIS/rest/services/Geometry/GeometryServer";
 
 var BASEMAP_SERVICE_NATGEO = "http://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer";
@@ -23,6 +24,17 @@ var BASEMAP_SERVICE_SATELLITE = "http://services.arcgisonline.com/ArcGIS/rest/se
 var _map;
 var _mapOV;
 var _scroll;
+
+var _locations;
+
+var _lutIconSpecs = {
+	normal:new IconSpecs(22,28,3,8),
+	medium:new IconSpecs(24,30,3,8),
+	large:new IconSpecs(32,40,3,11)
+}
+
+var ICON_PREFIX = "resources/icons/red/NumberIcon";
+var ICON_SUFFIX = ".png";
 
 var _dojoReady = false;
 var _jqueryReady = false;
@@ -66,8 +78,6 @@ function init() {
 		}
 	}
 	
-	_scroll = new iScroll('wrapper', {snap:'li',momentum:true});	
-	
 	// jQuery event assignment
 	
 	$(this).resize(handleWindowResize);
@@ -85,24 +95,6 @@ function init() {
 	$("#title").append(TITLE);
 	$("#subtitle").append(BYLINE);	
 	
-	$("li").click(function(e) 
-	{
-		if ($(this).find(".numberDiv").hasClass("selected")) {
-			$(this).find(".numberDiv").removeClass("selected");
-			$(this).find(".nameDiv").removeClass("selected");
-			$("#blot").animate({left:$("#case").width()});
-		} else {
-			$("li .nameDiv").removeClass("selected");
-			$("li .numberDiv").removeClass("selected");
-			$(this).find(".numberDiv").addClass("selected");
-			$(this).find(".nameDiv").addClass("selected");
-			var index = $.inArray(this,$("#scroller li"));
-			_scroll.scrollToPage(0, index, 500);			
-			$("#divInfo").empty();
-			$("#divInfo").append($(this).find(".nameDiv").html());			
-			setTimeout(function(){$("#blot").animate({left:40},"slow")}, 400);
-		}
-	});
 	
 	_map = new esri.Map("map");
 	_map.addLayer(new esri.layers.ArcGISTiledMapServiceLayer(BASEMAP_SERVICE_SATELLITE));
@@ -121,7 +113,61 @@ function init() {
 	mapDeferred.addCallback(function(response) {	  
 
 		_mapOV = response.map;
+				
+		_sourceLayer = $.grep(
+			response.itemInfo.itemData.operationalLayers,
+			function(n,i){
+				return $.trim(n.title).toLowerCase() == $.trim(LOCATIONS_LAYER_TITLE).toLowerCase()
+			})[0].featureCollection.layers[0];
+		
+		var locationsService = new LocationsParserService();
+		var numDiv;
+		var nameDiv;
+		var li;		  
 
+		locationsService.process(_sourceLayer.featureSet.features, function(locations){
+			_locations = locations;
+			var spec = _lutIconSpecs.normal;
+			$.each(_locations, function(index, value) {
+				value.setSymbol(new esri.symbol.PictureMarkerSymbol(
+					ICON_PREFIX+value.attributes.getRank()+ICON_SUFFIX, 
+					spec.getWidth(), 
+					spec.getHeight()).setOffset(spec.getOffsetX(), spec.getOffsetY())
+				);
+			   _mapOV.graphics.add(value);
+			   numDiv = $("<div class='numberDiv'>"+value.attributes.getRank()+"</div>");
+			   nameDiv = $("<div class='nameDiv'>"+value.attributes.getName()+"</div>");
+			   li = $("<li></li>");
+			   $(li).append(numDiv);
+			   $(li).append(nameDiv);
+			   $("#thelist").append(li);
+			});
+		});
+		
+		_scroll = new iScroll('wrapper', {snap:'li',momentum:true});	
+	
+			
+		$("li").click(function(e) 
+		{
+			if ($(this).find(".numberDiv").hasClass("selected")) {
+				$(this).find(".numberDiv").removeClass("selected");
+				$(this).find(".nameDiv").removeClass("selected");
+				$("#blot").animate({left:$("#case").width()});
+			} else {
+				$("li .nameDiv").removeClass("selected");
+				$("li .numberDiv").removeClass("selected");
+				$(this).find(".numberDiv").addClass("selected");
+				$(this).find(".nameDiv").addClass("selected");
+				/*
+				var index = $.inArray(this,$("#scroller li"));
+				_scroll.scrollToPage(0, index, 500);
+				*/			
+				$("#divInfo").empty();
+				$("#divInfo").append($(this).find(".nameDiv").html());			
+				setTimeout(function(){$("#blot").animate({left:40},"slow")}, 400);
+			}
+		});
+		
 		if(_mapOV.loaded){
 			initMap();
 		} else {
@@ -132,9 +178,6 @@ function init() {
 				
 	});
 	
-}
-
-function loaded() {
 }
 
 function initMap() {
@@ -157,7 +200,7 @@ function initMap() {
 	handleWindowResize();
 	
 	$("#case #blot").css("left", $("#case").width());
-	
+
 }
 
 function handleWindowResize() {
