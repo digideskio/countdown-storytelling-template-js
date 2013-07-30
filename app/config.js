611381a5863e18e4c5d48a4ed7d73daeef2a56c7
Lee Bock <lbock@esri.com>
2013-07-30 18:44:54 -0400

    added a comment to config

--- app/config.js
+++ app/config.js
@@ -5,6 +5,7 @@ define([],
 			//id for overview web map; this is the map that contains the content point layer
 			webmap_overview: "930979c835f44746b67538b9cd4d2d04",
 			//layer in overview webmap which provides the countdown content
+			//title should be readable from ArcGIS.com viewer TOC
 			contentLayer: "AAPA_PORT_RANKINGS", 
 			//id for satellite (or intended large scale) web map
 			webmap_largescale: "08c8c7870b2d44e7822548cb3cc6c057", 
